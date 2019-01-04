fc9bc17d892edefbcbe03034db9def9bbc3f9c46
tehrel langley <tehrellangley@gmail.com>
2019-01-03 20:17:35 -0500

    added conditional

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,8 +2,14 @@ class Product < ApplicationRecord
   has_many :orders
 
 
-def self.search(search_term)
-  Product.where("name LIKE ?", "%#{search_term}%")
-end
+  def self.search(search_term)
+    if "Rails.env.production?"
+      Product.where("name LIKE ?", "%#{search_term}%")
+   else
+      Product.where("name ilike ?", "%#{search_term}%")
 
-end
+
+    end
+
+  end
+end 
