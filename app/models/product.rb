743deb40651ff274fb1aad923a8b26c45697c5bd
tehrel langley <tehrellangley@gmail.com>
2019-01-04 17:11:09 -0500

    revised product

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,13 +3,16 @@ class Product < ApplicationRecord
 
 
   def self.search(search_term)
-    if "Rails.env.production?"
-      Product.where("name LIKE ?", "%#{search_term}%")
-   else
+
+    if Rails.env.production?
+
       Product.where("name ilike ?", "%#{search_term}%")
 
+   else
+
+      Product.where("name LIKE ?", "%#{search_term}%")
 
     end
 
   end
-end 
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'simple_pages/index'
   get 'simple_pages/about'
   get 'simple_pages/contact'
+  post 'simple_pages/thank_you'
   root 'simple_pages#landing_page'
   resources :orders, only: [:index, :show, :create, :destroy]
 
