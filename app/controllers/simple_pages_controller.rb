81efe172b1ed3fac589bdb781dbf2d183170199e
tehrel langley <tehrellangley@gmail.com>
2019-01-04 21:29:47 -0500

    added content

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -1,5 +1,18 @@
 class SimplePagesController < ApplicationController
   def landing_page
-     @products = Product.limit(9)
+    @products = Product.limit(9)
   end
-end
+  def thank_you
+    @name = params[:name]
+    @email = params[:email]
+    @message = params[:message]
+    ActionMailer::Base.mail(from: @email,
+      to: 'tehrellangley@example.com',
+      subject: "A new contact form message from #{@name}",
+      body: @message).deliver_now
+    end
+
+
+
+
+  end
