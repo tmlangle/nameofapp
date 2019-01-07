0671613705f4b0c8ee792ba779bebfb6c61581ab
tehrel langley <tehrellangley@gmail.com>
2019-01-07 17:24:40 -0500

    changed mailer

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,14 @@
+class UserMailer < ApplicationMailer
+
+  def thank_you
+  @name = params[:name]
+  @email = params[:email]
+  @message = params[:message]
+  UserMailer.contact_form(@email, @name, @message).deliver_now
+end
+
+
+
+
+
+end
