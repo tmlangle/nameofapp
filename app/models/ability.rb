016e3d534c1222d879c207cfbee4f26650ce7b4c
tehrel langley <tehrellangley@gmail.com>
2019-02-28 09:16:21 -0500

    added change to ability.rb

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,7 +4,7 @@ class Ability
     user ||= User.new # guest user (not logged in)
     can :manage, User, id: user.id
 
-    if user.role?(:admin)
+    if user.admin?
     can :manage, :destroy, :all
     end
   end
