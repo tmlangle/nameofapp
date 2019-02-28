1cc918025ac9966b10716b29d86c27b38e7195c1
tehrel langley <tehrellangley@gmail.com>
2019-02-28 09:43:19 -0500

    added automatic email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,9 +5,13 @@ class UserMailer < ApplicationMailer
       to: 'your-email@example.com',
       subject: "A new contact form message from #{name}")
     end
+    def welcome(user)
+      @appname = "Hippie Turtle"
+      mail(to: user.email,
+        subject: "Welcome to #{@appname}!")
+      end
 
 
 
-    
 
-  end
+    end
