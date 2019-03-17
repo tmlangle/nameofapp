3a59a59f2d5ab9c15a042330239457bdf41f339b
tehrel langley <tehrellangley@gmail.com>
2019-03-17 15:25:28 -0400

    customized checkout stripe form

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,15 +1,26 @@
 class PaymentsController < ApplicationController
   def create
+    @product = params[:product_id]
+    @user = params [:current_user]
     token = params[:stripeToken]
     # Create the charge on Stripe's servers - this will charge the user's card
     begin
-      charge = Stripe::Charge.create(
-        amount: 2000, # amount in cents, again
+      charge = Stripe::Charge.create({
+        amount: @amount, # amount in cents, again
         currency: "usd",
         source: token,
         description: params[:stripeEmail]
-      )
+        receipt_email: 'jenny.rosen@example.com',
+      })
+      if charge.paid
+        Order.create params[:product_id,:user_id,:total]
+      end
+
     rescue Stripe::CardError => e
       # The card has been declined
+      body = e.json_body
+      err = body[:error]
+      flash[:error] = "Unfortunately, there was an error processing your payment: #{err[:message]}"
     end
-end
+    redirect_to product_path(product)
+  end
