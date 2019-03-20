97665274ddde84e91c548fbbfcd4ba960732e249
tehrel langley <tehrellangley@gmail.com>
2019-03-19 21:25:19 -0400

    fixed redis.rb file

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,5 +1,5 @@
 if Rails.env.development?
-  $redis = Redis.new(host: 'localhost', port: 63790])
+  $redis = Redis.new(host: 'localhost', port: 6379)
   else
-    $redis = Redis.new(url: ENV["REDIS_URL"])
+   $redis = Redis.new(url: ENV["REDIS_URL"])
   end
