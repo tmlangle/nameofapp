20eea74c1b2e9d7581bdc60e7d41a0afcddbf422
tehrel langley <tehrellangley@gmail.com>
2019-03-21 12:44:43 -0400

    edited redis.rb

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,5 +1,13 @@
+require 'redis'
+
 if Rails.env.development?
-  $redis = Redis.new(host: 'localhost', port: 6379)
-  else
-   $redis = Redis.new(url: ENV["REDIS_URL"])
-  end
+  redis = Redis.new(host: 'localhost', port: 6379)
+else Rails.env.production?
+  redis = Redis.new(url: ENV["REDIS_URL"])
+end
+
+redis.set("mykey", "hello world")
+# => "OK"\\
+
+redis.get("mykey")
+# => "hello world"
